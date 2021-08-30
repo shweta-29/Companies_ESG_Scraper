2fc78f254440faf391270ab6c1dfcbe7fcb0e774
Shweta Yadav <shweta.yadav2092@gmail.com>
2021-08-30 22:51:39 +0100

    Changes for pypi

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='Companies ESG metrics',
-    version='0.0.1',
+    name="Companies' ESG metrics",
+    version='1.0.0',
     description='Package that allows you to find ESG ratings from Yahoo\
     Finance, MSCI, CSR Hub, S&P Global, SustainAnalytics. In addition,\
     financial information is also scraped from Yahoo Finance',
